bbf14039c396767e1c8aec1c7fb8916e9cc2fd22
Oficinas Unimetrocamp <oficina@acad.dvuadmin.net>
2019-06-26 21:42:59 -0300

    Introdução a conexão do banco

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://capacitacao-api-unimetrocamp:tasks@capacitacao-api-unimetrocamp-ty6jg.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 const app = express();
+
 
 app.use('/', require('./routes'));
 
