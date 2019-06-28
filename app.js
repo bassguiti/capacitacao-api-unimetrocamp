ebab90fc8f2419a8d3536e333422d73014281a4b
Oficinas Unimetrocamp <oficina@acad.dvuadmin.net>
2019-06-28 20:55:01 -0300

    Finalização da API

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://capacitacao-api-unimetrocamp:tasks@capacitacao-api-unimetrocamp-ty6jg.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://localhost:27017/capacitacao-api-unimetrocamp', {
     useNewUrlParser: true
 });
 const app = express();
 
+app.use(express.json());
 
 app.use('/', require('./routes'));
 
